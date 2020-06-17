08df2f9590cd37130354e07af8439d6864b570a7
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2020-06-17 09:42:24 +0530

    bug fix

--- distribution/scripts/jmeter/run-micro-gw-performance-tests.sh
+++ distribution/scripts/jmeter/run-micro-gw-performance-tests.sh
@@ -27,7 +27,7 @@ function initialize() {
     export apim_ssh_host=apim
     export apim_host=$(get_ssh_hostname $apim_ssh_host)
     echo "Downloading tokens to $HOME."
-    scp $apim_ssh_host:apim/micro-gw/target/jwt-tokens.csv $HOME/
+    scp $apim_ssh_host:apim/target/jwt-tokens.csv $HOME/
     scp $apim_ssh_host:apim/target/tokens.csv $HOME/
     if [[ $jmeter_servers -gt 1 ]]; then
         for jmeter_ssh_host in ${jmeter_ssh_hosts[@]}; do
