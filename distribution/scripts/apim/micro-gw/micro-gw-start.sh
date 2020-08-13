aa0870e67c71299851ab2a712828512cd27904cc
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2020-08-13 12:02:29 +0530

    change script to change number of threads to be cores * 4

--- distribution/scripts/apim/micro-gw/micro-gw-start.sh
+++ distribution/scripts/apim/micro-gw/micro-gw-start.sh
@@ -140,10 +140,14 @@ echo "Starting Microgateway"
 # )
 # popd
 
+NUMBER_OF_CORES=$(nproc --all) 
+MULTIPLY_FACTOR=4
+
+export BALLERINA_MAX_POOL_SIZE=$(($NUMBER_OF_CORES * $MULTIPLY_FACTOR))
+
 pushd runtime-mgw/bin
 (
     chmod a+x gateway
-    export BALLERINA_MAX_POOL_SIZE=100
     bash gateway /home/ubuntu/${label}/target/${label}.jar >/dev/null &
 )
 popd
