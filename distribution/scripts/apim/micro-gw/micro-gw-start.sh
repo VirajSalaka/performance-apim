e69df9717cad3acb31930ba4abb6dbcdde9db948
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2020-08-13 14:09:05 +0530

    change script to change number of threads to be cores * 8

--- distribution/scripts/apim/micro-gw/micro-gw-start.sh
+++ distribution/scripts/apim/micro-gw/micro-gw-start.sh
@@ -141,7 +141,7 @@ echo "Starting Microgateway"
 # popd
 
 NUMBER_OF_CORES=$(nproc --all) 
-MULTIPLY_FACTOR=4
+MULTIPLY_FACTOR=8
 
 export BALLERINA_MAX_POOL_SIZE=$(($NUMBER_OF_CORES * $MULTIPLY_FACTOR))
 
